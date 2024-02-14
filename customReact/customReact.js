ddbce396129053109dfbd44926b86617ab33ce82
Aman Kumar <aman.kr0100@gmail.com>
2024-02-14 23:43:31 +0530

    moduler custom react code

--- customReact/customReact.js
+++ customReact/customReact.js
@@ -1,10 +1,20 @@
 function customRender(reactElement,container){
-    const domElement = document.createElement(reactElement.type);
-    domElement.innerHTML = reactElement.children;
-    domElement.setAttribute('href',reactElement.props.href)
-    domElement.setAttribute('target',reactElement.props.target);
+    // const domElement = document.createElement(reactElement.type);
+    // domElement.innerHTML = reactElement.children;
+    // domElement.setAttribute('href',reactElement.props.href)
+    // domElement.setAttribute('target',reactElement.props.target);
 
-    container.appendChild(domElement);
+    // container.appendChild(domElement);
+//    ------------------------
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if(prop == 'children') continue
+
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+    // ----------------------
 }
 const reactElement = {
     type: 'a',
@@ -17,3 +27,5 @@ const reactElement = {
 const mainContainer = document.querySelector('#root');
 
 customRender(reactElement, mainContainer);
+
+//------------ use loop base code for modular work

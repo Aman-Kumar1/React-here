f573e0fea528b0a9aa0a0aa8b59fb4e748aff9b5
Aman Kumar <aman.kr0100@gmail.com>
2024-02-15 10:04:11 +0530

    Small project with Hooks(useState) counter

---
+++ 02Counter/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+    // useState(true) //we have to add default value for this functio (true) , (empty array) or number somthing
+
+    let [counter, setCounter] =  useState(0); //this is a method counter is only nameing convection "setCounter is function"
+    
+  // let counter = 5; no working here we have to use HOOKS
+
+  let addValue= () => {
+    // console.log("click",counter);
+    // counter = counter + 1 ;
+    if(counter < 20){
+      setCounter(counter + 1);
+    }
+    else{
+      alert("not more then 20");
+    }
+  }
+
+  let minusValue = () => {
+    if(counter > 0){
+      setCounter(counter - 1);
+    }
+    else{
+      alert("not less then 0");
+      }
+    }
+  return (
+    <>
+     
+      <h1>Vite + React</h1>
+      <h2>counter value: {counter}</h2>
+      <button onClick={addValue}>add value</button>
+      <br />
+      <button onClick={minusValue}>minus value</button>
+    </>
+  )
+}
+
+export default App
